c08b5186b68f8e8ac65732efe469624e30e3e4dc
Saint-Jean Germila <germila@noukod.com>
2018-12-06 17:15:55 +0000

    Done.

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,9 +2,10 @@ class CreateShows < ActiveRecord::Migration
   def change
     create_table :shows do |t|
       t.string :name
-      t.string :network
       t.string :day
+      t.string :network
       t.integer :rating
     end
+    #add_column :shows, :season, :string
   end
 end
